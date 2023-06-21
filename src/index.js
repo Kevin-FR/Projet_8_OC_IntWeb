81fb6bdf5ffcc0070b27b570626671392ecf9bf2
Kévin M <neocorp_fr@hotmail.com>
2023-06-21 13:43:58 +0200

    clear packages

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import About from "./pages/About";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -27,7 +26,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
